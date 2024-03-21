08579d87387089a5d73dbb01ae5b7ffbad05c6cc
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2024-03-20 20:44:54 -0400

    subindo projeto ruby on rails

---
+++ db/migrate/20240320231126_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
